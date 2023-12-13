d2baf8202f9f95c6f506478f925d339c5493fbaf
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-13 14:03:53 +0100

    feat: add async iterator conversion for sync iterator in stream

---
+++ stream/convert-to-stream/03-sync-iterator-to-async-iterator.js
@@ -0,0 +1,18 @@
+import { Readable } from 'node:stream'
+
+// * Stream are async iterators
+function* generate() {
+  for (let i = 1; i < 1025; i++) {
+    yield i
+  }
+}
+
+async function run() {
+  // convert sync iterator to async iterator!
+  const rs = Readable.from(generate())
+  for await (const chunk of rs) {
+    console.log(chunk)
+  }
+}
+
+run()
