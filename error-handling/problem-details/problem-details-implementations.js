ead44090ca970ba4485ea10058605679cf5682c2
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-12-30 21:15:04 +0100

    feat: add problem details implementations for error handling

---
+++ error-handling/problem-details/problem-details-implementations.js
@@ -0,0 +1,9 @@
+// source: [RFC 7807 - Problem Details for HTTP APIs](https://datatracker.ietf.org/doc/html/rfc7807)
+// article:
+//  - [Send awesome structured error responses with Express - Simon Plenderleith](https://simonplend.com/send-awesome-structured-error-responses-with-express/)
+// library:
+//  - [PDMLab/http-problem-details: This library implements HTTP Problem details (RFC 7807) for HTTP APIs build with Node.js.](https://github.com/PDMLab/http-problem-details)
+// express example:
+//  - [PDMLab/express-http-problem-details: Express middleware to create HTTP Problem Details (RFC7807) by convention](https://github.com/PDMLab/express-http-problem-details)
+class RequestHandler {}
+class ProblemDetailsError {}
