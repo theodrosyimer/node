6755a4aebb0834f371f2457ecf621617693a3208
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-04 14:24:15 +0100

    feat: add promise-shared-response.ts file with error handling

---
+++ misc/promise-shared-response.ts
@@ -0,0 +1,16 @@
+import {scheduler} from 'timers/promises'
+
+let count = 0
+
+async function maybeThrow(){
+  await scheduler.wait(1000)
+  if (Math.random() > 0.5) {
+    throw new Error('Rejected')
+  }
+  return `Fulfilled ${count++}`
+}
+
+let p = await maybeThrow()
+console.log(p)
+console.log(p)
+console.log(p)
