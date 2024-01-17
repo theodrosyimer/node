d84d29026c509bd93949b1efb649a1bf022cd67b
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-17 18:47:03 +0100

    feat: update fs/readFile-promise.js with openToReadFile function

--- fs/readFile-promise.js
+++ fs/readFile-promise.js
@@ -1,9 +1,9 @@
-import { readFile } from 'fs/promises'
+import { open } from 'fs/promises'
 import { join } from 'path'
-import { isMainModule } from '../url/is-file-main-module.js'
+
 import { getDirname } from '../url/get-dirname.js'
 import { getFilename } from '../url/get-filename.js'
-import { readFromJsonFile } from './readFromJsonFile.js'
+import { isMainModule } from '../url/is-file-main-module-esm.js'
 
 console.log(getDirname(import.meta.url, 'data.json'))
 console.log(getFilename(import.meta.url))
@@ -14,9 +14,24 @@ console.log(isMainModule(import.meta.url))
 console.log(join(process.env.NODE_PATH ?? ''))
 console.log(join(process.env.NODE_PATH ?? '', 'data.json'))
 
-readFromJsonFile(
-  join(process.env.NODE_PATH ?? '', 'data.json'),
-  (fileContent) => {
-    console.log(fileContent)
-  },
-)
+async function openToReadFile(path, flags = 'r+', mode = 0o666) {
+  let filehandle
+
+  try {
+    filehandle = await open(path, flags, mode)
+
+    const content = await filehandle.readFile({ encoding: 'utf8' })
+
+    return content
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      console.error(`"${path}" already exists`)
+      return error
+    }
+    throw error
+  } finally {
+    if (filehandle) {
+      await filehandle.close()
+    }
+  }
+}
