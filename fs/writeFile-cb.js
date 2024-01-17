b950aa3af9f64a5a46d6bd78ba0e5247054b7753
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-17 18:46:19 +0100

    feat: update writeFile-cb.js with new import and writeToFile function

--- fs/writeFile-cb.js
+++ fs/writeFile-cb.js
@@ -1,6 +1,8 @@
+/* eslint-disable camelcase */
 import { open, close, writeFile } from 'fs'
 import { join } from 'path'
-import { isMainModule } from '../url/is-file-main-module.js'
+
+import { isMainModule } from '../url/is-file-main-module-esm.js'
 
 console.log(isMainModule(import.meta.url))
 
@@ -8,36 +10,36 @@ console.log(isMainModule(import.meta.url))
 // `NODE_PATH=. node app.js`
 console.log(join(process.env.NODE_PATH ?? '', 'data.json'))
 
-await writeToFile('./data3.json', '')
+writeToFile('./data3.json', {
+  id: 11,
+  first_name: 'Orsa',
+  last_name: 'Montrose',
+  email: 'omontrose9@sogou.com',
+  gender: 'Female',
+  ip_address: '181.147.69.70',
+})
 
 /**
  *
  * @param {import('fs').PathLike | import('fs').FileHandle} path
  * @param {{ flags?: 'a' | 'ax' | 'a+' | 'ax+' | 'as' | 'r' | 'rs' | 'r+' | 'rs+' | 'w' | 'wx' | 'w+' | 'wx+'; mode?: import('fs').Mode | null | undefined }} options flags default: 'wx', mode default: 0o666
  */
-async function writeToFile(path, { flags = 'wx', mode = 0o666 }) {
-  open(path, flags, mode, (err, fd) => {
-    if (err) {
-      if (err.code === 'EEXIST') {
+function writeToFile(path, data, { flags = 'wx', mode = 0o666 }) {
+  open(path, flags, mode, (error, fd) => {
+    if (error) {
+      if (error.code === 'EEXIST') {
         console.error(`${path} already exists`)
         return
       }
 
-      throw err
+      throw error
     }
 
     try {
-      writeDataToFile(fd, {
-        id: 11,
-        first_name: 'Orsa',
-        last_name: 'Montrose',
-        email: 'omontrose9@sogou.com',
-        gender: 'Female',
-        ip_address: '181.147.69.70',
-      })
+      writeDataToFile(fd, data)
     } finally {
-      close(fd, (err) => {
-        if (err) throw err
+      close(fd, error => {
+        if (error) throw error
       })
     }
   })
@@ -47,16 +49,16 @@ async function writeToFile(path, { flags = 'wx', mode = 0o666 }) {
 
 /**
  *
- * @param {import('fs').PathOrFileDescriptor} pathOrFileDescriptor
- * @param {string | NodeJS.ArrayBufferView} object
+ * @param {import('fs').PathOrFileDescriptor} path
+ * @param {string | NodeJS.ArrayBufferView} data
  */
-export function writeDataToFile(pathOrFileDescriptor, object) {
+export function writeDataToFile(path, data) {
   writeFile(
-    pathOrFileDescriptor,
-    typeof object === 'object' ? JSON.stringify(object) : object,
-    (err) => {
-      if (err) {
-        console.error(err)
+    path,
+    typeof data === 'object' ? JSON.stringify(data) : data,
+    error => {
+      if (error) {
+        console.error(error)
       }
       console.log('The file has been saved!')
     },
