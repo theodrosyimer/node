24f36f11b1174c9cecffb76310750ff894f3dc6f
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-20 21:24:54 +0100

    feat: add error handling and comments for abort signal

--- readline/readline-abort.js
+++ readline/readline-abort.js
@@ -12,12 +12,14 @@ async function* generatePrompt() {
 
   const signal = AbortSignal.timeout(3_000)
 
+  // * aborting will exit the current process
   signal.addEventListener(
     'abort',
     () => {
       console.log('\n\nThe food question timed out')
       console.log(`\nBye 👋🏿`)
-      process.exit(0)
+      // comment this to test the error handling of `error.code === 'ABORT_ERR'`
+      // process.exit(0)
     },
     { once: true },
   )
@@ -44,24 +46,44 @@ async function* generatePrompt() {
       yield `\nAI: ${aiResponse}`
     }
   } catch (error) {
-    if (error.code === 'ABORT_ERR') {
-      console.error(
-        'ABORTED!\nYou need to handle the signal properly, it seems like you forgot to exit the process.\nAdd `process.exit(exitCode)` at the end of your callback.',
-      )
+    // 1. check if it is indeed an `Error` instance...
+    if (error instanceof Error) {
+      // this is an example of the use of a known error code
+      if (error.code === 'ABORT_ERR') {
+        console.error(
+          'ABORTED!\nYou need to handle the signal properly, it seems like you forgot to exit the process.\nAdd `process.exit(exitCode)` at the end of your callback.',
+        )
+      }
+      // if there are error codes use it, an example for checking if there is any (only useful during development)
+      if (error.code) {
+        // so it will be log here, now i can use the `error.code` to handle a particular use case if needed
+        console.log(`ERROR CODE: ${error.code}`)
+        console.log(`ERROR STACK: ${error.stack}`)
+        return // ! handle it properly (just an example)
+      }
+      // but there are not always `error.code`...
+      if (error.name) {
+        // so it will be log here, now i can use the `error.code` to handle a particular use case if needed
+        console.log(error.stack)
+      }
     }
+
+    // 2. ...otherwise do something with the error
+    console.log(error)
   }
 }
+
 async function startPrompt(iterator) {
   for await (const line of iterator) {
     console.log(line)
   }
 }
 
-async function app(prompt) {
+async function main(prompt) {
   try {
-    await startPrompt(prompt)
+    await startPrompt(prompt())
   } catch (error) {
-    console.log('AppError:', error)
+    console.error('AppError:', error)
   } finally {
     rl.close()
   }
@@ -72,4 +94,4 @@ rl.on('SIGINT', () => {
   process.exit(0)
 })
 
-app(generatePrompt())
+main(generatePrompt)
