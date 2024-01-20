6ee2ccddd66466e14f3fbade53c5d79301d79259
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-20 21:24:45 +0100

    feat: refactor readline.js to handle errors and improve code structure

--- readline/readline.js
+++ readline/readline.js
@@ -10,22 +10,47 @@ const rl = readline.createInterface({
 async function* generatePrompt() {
   // const chatMessages = []
 
-  while (true) {
-    const userInput = await rl.question('\nYou: ')
+  try {
+    while (true) {
+      const userInput = await rl.question('\nYou: ')
 
-    // chatMessages.push({
-    //   role: 'user',qrr
-    //   content: userInput,
-    // })
+      // chatMessages.push({
+      //   role: 'user',qrr
+      //   content: userInput,
+      // })
 
-    const aiResponse = 'Oh yeah!'
+      const aiResponse = 'Oh yeah!'
 
-    // chatMessages.push({
-    //   role: 'user',
-    //   content: aiResponse,
-    // })
+      // chatMessages.push({
+      //   role: 'user',
+      //   content: aiResponse,
+      // })
 
-    yield `\nAI: ${aiResponse}`
+      yiel`\nAI: ${aiResponse}`
+    }
+  } catch (error) {
+    // check if it is indeed an `Error` instance
+    if (error instanceof Error) {
+      // this is an example of the use of a known error code
+      if (error.code === 'ABORT_ERR') {
+        console.error(
+          'ABORTED!\nYou need to handle the signal properly, it seems like you forgot to exit the process.\nAdd `process.exit(exitCode)` at the end of your callback.',
+        )
+      }
+      // if there are error codes use it, an example for checking if there is any (only useful during development)
+      if (error.code) {
+        // so it will be log here, now i can use the `error.code` to handle a particular use case if needed
+        console.log(error.code)
+      }
+      // but there are not always `error.code`...
+      if (error.name) {
+        // so it will be log here, now i can use the `error.code` to handle a particular use case if needed
+        console.log(error.stack)
+      }
+    }
+
+    // otherwise do something with the error
+    console.log(error)
   }
 }
 async function startPrompt(iterator) {
@@ -36,7 +61,7 @@ async function startPrompt(iterator) {
 
 async function app(prompt) {
   try {
-    await startPrompt(prompt)
+    await startPrompt(prompt())
   } catch (error) {
     console.log('ERROR', error)
   } finally {
@@ -49,4 +74,4 @@ rl.on('SIGINT', () => {
   process.exit(0)
 })
 
-app(generatePrompt())
+app(generatePrompt)
