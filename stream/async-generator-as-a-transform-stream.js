35a9de18448849b0f19d582cc038ee5725483671
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-20 21:24:33 +0100

    feat: add async generator as a transform stream

---
+++ stream/async-generator-as-a-transform-stream.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { createReadStream, createWriteStream } from 'node:fs'
+import { pipeline as pipelinePromise } from 'node:stream/promises'
+
+await pipelinePromise(
+  // createReadStream('lowercase.txt'),
+  process.stdin,
+  async function* (source, { signal }) {
+    source.setEncoding('utf8') // Work with strings rather than `Buffer`s.
+    for await (const chunk of source) {
+      yield chunk.toUpperCase()
+    }
+  },
+  // createWriteStream('uppercase.txt'),
+  process.stdout,
+)
