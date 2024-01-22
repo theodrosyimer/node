f405f7ab6a6175f7706397de33b491eaa7c776e3
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-22 04:53:58 +0100

    refactor: refactor async generator as a transform stream

--- stream/async-generator-as-a-transform-stream.js
+++ stream/async-generator-as-a-transform-stream.js
@@ -2,10 +2,13 @@
 import { createReadStream, createWriteStream } from 'node:fs'
 import { pipeline as pipelinePromise } from 'node:stream/promises'
 
+// const ac = new AbortController()
+// const { signal } = ac
+
 await pipelinePromise(
   // createReadStream('lowercase.txt'),
   process.stdin,
-  async function* (source, { signal }) {
+  async function* (source /* { signal } */) {
     source.setEncoding('utf8') // Work with strings rather than `Buffer`s.
     for await (const chunk of source) {
       yield chunk.toUpperCase()
