f83c3ad34d50d87d5fa01b28bcb12e1aa49e705b
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-03-24 13:41:22 +0100

    move to a folder

---
+++ shell/run-shell-command.js
@@ -0,0 +1,14 @@
+// source: node child_process.exec's documentation
+import { promisify } from 'node:util'
+import { exec as _exec } from 'node:child_process'
+
+const exec = promisify(_exec)
+
+async function lsExample() {
+  const { stdout, stderr } = await exec('ls')
+  // console.log(`stdout:\n\n${stdout}`)
+  // console.error(`stderr:\n\n${stderr}`)
+  return stdout || stderr
+}
+
+await lsExample()
