8454b7658d4e572c0c464fdc45658833a13eb278
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-08-21 16:00:21 +0200

    refactor: Update is-file-main-module-esm.js to use import.meta

--- url/is-file-main-module-esm.js
+++ url/is-file-main-module-esm.js
@@ -1,16 +1,28 @@
+#!/usr/bin/env node
+
+import path from 'path'
 import { fileURLToPath } from 'url'
 
 // inspiration: [Use case for URLs: detecting if the current module is “main” (the app entry point)](https://exploringjs.com/nodejs-shell-scripting/ch_nodejs-path.html#detecting-if-module-is-main)
 /**
- * @param {string} fileURL
- * @returns
+ * Check if a module is run directly with node or is imported
+ * from another module.
+ * @param {ImportMeta} meta The `import.meta` object.
+ * @return {boolean} The module is run directly.
  */
-export function isMainModule(fileURL) {
-  if (fileURL.startsWith('file:')) {
-    const modulePath = fileURLToPath(fileURL)
-    if (process.argv[1] === modulePath) {
-      return true
-    }
+export default function isEntryPoint(meta) {
+  if (!meta || !process.argv[1]) {
+    return false
   }
-  return false
+
+  const filePath = process.argv[1]
+  const modulePath = fileURLToPath(meta.url)
+  const modulePathNoExtension = modulePath.substring(
+    0,
+    modulePath.lastIndexOf('.'),
+  )
+
+  return filePath === modulePath || filePath === modulePathNoExtension
 }
+
+console.log(isEntryPoint(import.meta))
