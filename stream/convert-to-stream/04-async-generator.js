67192e7b650e51b76d816d0211a9f2f87f674995
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-08-21 15:54:14 +0200

    refactor: Convert sync iterator to async iterator in stream conversion functions

--- stream/convert-to-stream/04-async-generator.js
+++ stream/convert-to-stream/04-async-generator.js
@@ -1,16 +1,13 @@
-import { Readable } from 'node:stream'
-
 async function* generate() {
   for (let i = 1; i < 1025; i++) {
     yield i
   }
 }
 
-async function run() {
-  const rs = Readable.from(generate())
-  for await (const chunk of rs) {
+async function consume(iterator) {
+  for await (const chunk of iterator) {
     console.log(chunk)
   }
 }
 
-run()
+consume(generate())
