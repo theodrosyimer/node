36dceb09d60f0b9d747711540893cc2bc5f43d62
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2026-03-02 12:36:45 +0100

    refactor: change console.log to console.error

--- fs/access.js
+++ fs/access.js
@@ -22,22 +22,22 @@ const file = 'package.json'
 
 // Check if the file exists in the current directory.
 access(file, constants.F_OK, err => {
-  console.log(`${file} ${err ? 'does not exist' : 'exists'}`)
+  console.error(`${file} ${err ? 'does not exist' : 'exists'}`)
 })
 
 // Check if the file is readable.
 access(file, constants.R_OK, err => {
-  console.log(`${file} ${err ? 'is not readable' : 'is readable'}`)
+  console.error(`${file} ${err ? 'is not readable' : 'is readable'}`)
 })
 
 // Check if the file is writable.
 access(file, constants.W_OK, err => {
-  console.log(`${file} ${err ? 'is not writable' : 'is writable'}`)
+  console.error(`${file} ${err ? 'is not writable' : 'is writable'}`)
 })
 
 // Check if the file is readable and writable.
 access(file, constants.R_OK | constants.W_OK, err => {
-  console.log(`${file} ${err ? 'is not' : 'is'} readable and writable`)
+  console.error(`${file} ${err ? 'is not' : 'is'} readable and writable`)
 })
 
 ///////////////////////////////////////////////////////////////////////
