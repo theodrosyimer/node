6d319315722f1e4027ed1ed12f755205bae6582a
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2026-03-02 12:35:32 +0100

    refactor: Update transform-stream-async-generator

--- stream/transform-stream-async-generator.js
+++ stream/transform-stream-async-generator.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 // import { createReadStream, createWriteStream } from 'node:fs'
-import { pipeline as pipelinePromise } from 'node:stream/promises'
+import { pipeline } from 'node:stream/promises'
 
-// const ac = new AbortController()
-// const { signal } = ac
+const ac = new AbortController()
+const { signal } = ac
 
-await pipelinePromise(
+await pipeline(
   // createReadStream('lowercase.txt'),
   process.stdin,
-  async function* (source /* { signal } */) {
+  async function* (source, { signal }) {
     source.setEncoding('utf8') // Work with strings rather than `Buffer`s.
     for await (const chunk of source) {
       yield chunk.toUpperCase()
